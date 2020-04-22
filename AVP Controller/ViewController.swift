59d84d9ab7ede7b2026d3aeb80115df7c7ec6f92
wilson lo <lowilson180@gmail.com>
2020-04-22 09:50:04 +0800

    Initial Commit

---
+++ AVP Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AVP Controller
+//
+//  Created by 啟發電子 on 2020/4/22.
+//  Copyright © 2020 gomax. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
